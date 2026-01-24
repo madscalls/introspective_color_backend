e7698e47a054db99a03ef11532de0799ea87be7b
Madss Calls <madscalls@gmail.com>
2026-01-23 23:04:30 -0500

    post fix

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const mongoose = require("mongoose");
-
 const postSchema = new mongoose.Schema(
   {
     imageUrl: { type: String, required: true },
