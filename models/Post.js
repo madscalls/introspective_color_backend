d0619e4c66091f37ff735bae95feb44d39881a8f
Madss Calls <madscalls@gmail.com>
2026-01-23 23:32:53 -0500

    post.js

--- models/Post.js
+++ models/Post.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 const postSchema = new mongoose.Schema(
   {
     imageUrl: { type: String, required: true },
-    publicId: { type: String, default: "" },
-    color: {
-      type: String,
+    publicId: { type: String, required: true },
+    color: { type: String, required: true },
+    hashtags: { type: String, default: "" },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
-      enum: ["red", "orange", "yellow", "green", "blue", "purple"],
     },
-    hashtags: { type: [String], default: [] },
   },
   { timestamps: true },
 );
